b3a5eb0d0a2b2307375a3ae62b1b3a2c21652202
Hassan A <has_is_ere@hotmail.com>
2018-07-13 15:30:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 function takeANumber (katzDeliLine, name){
-  console.log(katzDeliLine)
   katzDeliLine.push(name)
   var numberInLine = katzDeliLine.indexOf(name)
-  numberInLine = numberInLine
-  console.log(numberInLine)
-  console.log(typeof(numberInLine))
- 
   return `Welcome, ${name}. You are number ${numberInLine} in line.`
 }
