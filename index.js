e3bf13e6f944b21cd2144dd34a21dee4fddb833c
Hassan A <has_is_ere@hotmail.com>
2018-07-13 15:24:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber (katzDeliLine, name){
   var numberInLine = katzDeliLine.indexOf(name)
-  var numberInLine = numberInLine + 1
+  numberInLine = numberInLine + 1
   return `Welcome, ${name}. You are number ${numberInLine} in line.`
 }
